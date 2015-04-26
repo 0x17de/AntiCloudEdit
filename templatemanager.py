1e41b2b15d3eac130432e894ff9a863959ba4dca
Manuel Herrmann <it@icetruck.de>
2015-04-26 19:08:50 +0200

    removed unnecessary template manager

--- templatemanager.py
+++
@@ -1,9 +0,0 @@
-
-class TemplateManager():
-	templates = {}
-	vars = {}
-	def add(self, name, file):
-		self.templates[name] = open(file, "r").read()
-	
-	def get(self, name):
-		return self.templates[name]
